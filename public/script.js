422d178d236309d4f675f64b4cc9d2415b26fb51
El Professor <ghanam4474@gmail.com>
2023-12-07 01:07:45 +0500

    Update script.js to handle pasted values

--- public/script.js
+++ public/script.js
@@ -1,5 +1,9 @@
 async function calculateSalary() {
-  const salesInput = document.getElementById("salesInput").value;
+  const salesInput = document
+    .getElementById("salesInput")
+    .value.trim()
+    .replace(/\s+/g, " ");
+
   try {
     const response = await fetch("/calculate", {
       method: "POST",
@@ -22,3 +26,22 @@ async function calculateSalary() {
     console.error("Error during fetch:", error);
   }
 }
+
+// Add an event listener for the paste event
+document
+  .getElementById("salesInput")
+  .addEventListener("paste", function (event) {
+    // Prevent the default paste behavior
+    event.preventDefault();
+
+    // Get the pasted text
+    var pastedText = (event.clipboardData || window.clipboardData).getData(
+      "text"
+    );
+
+    // Remove line breaks and trim spaces
+    pastedText = pastedText.replace(/\s+/g, " ").trim();
+
+    // Set the modified text back to the input
+    document.getElementById("salesInput").value = pastedText;
+  });
